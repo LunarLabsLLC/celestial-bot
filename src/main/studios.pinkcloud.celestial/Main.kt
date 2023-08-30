3c008e582633e0d2828b6d802722747133d8bb1f
egirldevs <emily@Bunni.me>
2023-08-30 17:51:19 -0400

    xd

--- src/main/studios.pinkcloud.celestial/Main.kt
+++ src/main/studios.pinkcloud.celestial/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val kord = Kord("your bot token")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    kord.login()
 }
